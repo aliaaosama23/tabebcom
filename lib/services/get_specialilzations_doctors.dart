8870ed35f38cd67b4fa49fa67a26e82b30fe8a45
aliaaosama23 <engaliaaosama@gmail.com>
2021-10-22 18:42:50 +0200

    private

--- lib/services/get_specialilzations_doctors.dart
+++ lib/services/get_specialilzations_doctors.dart
@@ -7,7 +7,7 @@ Future<List<Doctor>> getDoctorsBySpeciality(http.Client client,
     speciality, double lat, double long) async {
   print('lat is $lat');
   print('long is $long');
-   var url='https://mcare.careofme.net/api/Doctor/GetDoctors?DoctorSpeciality=$speciality&Branch_ID=null&ClinicDept_Code=null&lat=$lat&long=$long&DistanceKilos=50';
+   var url='/api/Doctor/GetDoctors?DoctorSpeciality=$speciality&Branch_ID=null&ClinicDept_Code=null&lat=$lat&long=$long&DistanceKilos=50';
    final response = await client.get(
       Uri.parse(url));
       print(url);
